417a2308a5234946f9bc1deec1cdf5f6f3f8f08b
Pierre <pierre@exygy.com>
2013-11-14 10:56:21 -0800

    Create new rails project: Castro Theatre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CastroTheatre::Application.config.secret_key_base = '105fdd3e25b3e83bfac1ed6edffe3781dadde3f4422ac94bc30d95b017e4be7bffd2383d35eb524e1636a6d62995883cc266373259723e51152ffdcff07d4d38'
