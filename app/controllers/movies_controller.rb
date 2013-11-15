9f4058ce23b0db365b80e491ddcfdff8a7e09899
Pierre <pierre@exygy.com>
2013-11-15 10:24:03 -0800

    Display a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
+	before_filter :set_movie, only: [:show]
+
 	def index
 		@movies = Movie.all
 	end
+
+	def show
+	end
+
+	private
+	    # Use callbacks to share common setup or constraints between actions.
+	    def set_movie
+	      @movie = Movie.find(params[:id])
+	    end
+
 end
