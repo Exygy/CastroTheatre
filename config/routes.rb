25728880e218adb0d6f9275cb3af49a78287ff00
Pierre <pierre@exygy.com>
2013-11-15 09:56:51 -0800

    Display list of movies

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ CastroTheatre::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :movies, only:[:index]
+
   # Example resource route with options:
   #   resources :products do
   #     member do
