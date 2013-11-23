aa2cba448e1af74bd9fc6f64423cc6fd5f4ff805
Pierre <pierre@exygy.com>
2013-11-22 23:21:09 -0800

    add model relation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ActiveRecord::Base
 	belongs_to :floor
+	has_and_belongs_to_many :movies
 end
--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -9,4 +9,17 @@ class RoomTest < ActiveSupport::TestCase
 
     assert r.floor.id == f.id
   end
+
+  test "a room has many movies" do
+    r = rooms(:room1)
+    
+    m1 = movies(:superman)
+    m2 = movies(:spiderman)
+    
+    r.movies << m1
+    r.movies << m2
+
+    assert r.movies.collect{|m| m.id }.sort == [m1.id, m2.id].sort
+  end
+
 end
